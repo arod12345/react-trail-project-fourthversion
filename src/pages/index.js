b39c1e07ad736820f2a6144d9e86c8faf190af61
arod12345 <sisayabel681@gmail.com>
2023-06-15 02:31:25 +0300

    error fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import About from "./About";
 import Documents from "./Documents";
-import Home from "./home";
+import Home from "./Home";
 import Welcome from "./Welcome";
 
 export { About, Documents, Home, Welcome }
